2370275e1f05a83e06af4a45921fe8f5a9e9f5d4
Simon Nim <simonnim916@gmail.com>
2016-08-31 15:22:38 -0700

    add associations

--- db/migrate/20160831221719_create_workouts.rb
+++ db/migrate/20160831221719_create_workouts.rb
@@ -1,6 +1,8 @@
 class CreateWorkouts < ActiveRecord::Migration[5.0]
   def change
     create_table :workouts do |t|
+    	t.string :workout
+    	t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20160831221730_create_meals.rb
+++ db/migrate/20160831221730_create_meals.rb
@@ -1,6 +1,8 @@
 class CreateMeals < ActiveRecord::Migration[5.0]
   def change
     create_table :meals do |t|
+    	t.string :meal
+    	t.integer :user_id
 
       t.timestamps
     end
