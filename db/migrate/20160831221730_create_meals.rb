3935eeb302daf712b1274260cadba1625ed42038
Simon Nim <simonnim916@gmail.com>
2016-09-12 22:00:26 -0700

    fixed the meals db items

--- db/migrate/20160831221730_create_meals.rb
+++ db/migrate/20160831221730_create_meals.rb
@@ -2,6 +2,8 @@ class CreateMeals < ActiveRecord::Migration[5.0]
   def change
     create_table :meals do |t|
     	t.string :meal
+    	t.string :ingredients
+    	t.string :directions
     	t.integer :user_id
 
       t.timestamps
