e8c3ff12853768a1b19babe76612bd21066ee040
Calvin Kwong <ckwong93@gmail.com>
2016-09-19 14:28:10 -0700

    Add exercist list database with styling

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,6 +3,13 @@ class WorkoutsController < ApplicationController
 	def index
 		@user = User.find(params[:user_id])
 		@exerciselistbicep = Exerciselist.where(muscle: "biceps")
+		@exerciselisttricep = Exerciselist.where(muscle: "triceps")
+		@exerciselistchest = Exerciselist.where(muscle: "chest")
+		@exerciselistshoulder = Exerciselist.where(muscle: "shoulders")
+		@exerciselistabs = Exerciselist.where(muscle: "abdominals")
+		@exerciselistback = Exerciselist.where("muscle = ? or muscle = ?", "lower back", "upper back")
+		@exerciselistlegs = Exerciselist.where("muscle = ? or muscle = ? or muscle = ? or muscle = ?", "quadriceps", "hamstrings", "calves", "glutes")
+		
 	end
 
 	def new
