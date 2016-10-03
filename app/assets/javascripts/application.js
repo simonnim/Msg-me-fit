c69dc4bd557ef413938a37023b019a9fd83dbfe1
Calvin Kwong <ckwong93@gmail.com>
2016-10-03 13:18:23 -0700

    Finalize styling, responsive navbar, image overlays

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,3 +39,11 @@ $(document).ready(function(){
 });
 
 
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 150)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
+});
