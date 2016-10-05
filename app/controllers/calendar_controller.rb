37bb7afbb42a07145b9c22f2208455d5eeb7da31
Calvin Kwong <ckwong93@gmail.com>
2016-10-04 21:19:22 -0700

    edit calendar view page

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,6 +3,8 @@ class CalendarController < ApplicationController
 		@user = User.find(params[:user_id])
 		@user_id = current_user.id
 		@user_appointments = Calendar.where(user_id: @user_id)
+		@user_upcoming_appointments = @user_appointments.where("time > ?", Time.now)
+    @user_past_appointments = @user_appointments.where("time < ?", Time.now)
 	end
 	
 	def create
